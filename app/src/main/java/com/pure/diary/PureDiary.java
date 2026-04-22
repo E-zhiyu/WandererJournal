10d8fc3dfac5d6095b2994c71a2e6d35f9a0792e
E_zhiyu <E82487318@outlook.com>
2026-04-22 14:16:49 +0800

    创建项目并导入基本依赖

---
+++ app/src/main/java/com/pure/diary/PureDiary.java
@@ -0,0 +1,12 @@
+package com.pure.diary;
+
+import android.app.Application;
+
+public class PureDiary extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
