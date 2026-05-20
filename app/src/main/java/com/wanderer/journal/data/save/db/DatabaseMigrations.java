5fd145adaabc09063ad342ddfc1c58d9aca8293a
E_zhiyu <E82487318@outlook.com>
2026-05-20 10:01:07 +0800

    段落表新增段落 ID 的索引

--- app/src/main/java/com/wanderer/journal/data/save/db/DatabaseMigrations.java
+++ app/src/main/java/com/wanderer/journal/data/save/db/DatabaseMigrations.java
@@ -5,10 +5,19 @@ import androidx.room.migration.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
 public class DatabaseMigrations {
+    //情绪标签添加分类字段
     public static final Migration MIGRATION_1_2 = new Migration(1, 2) {
         @Override
         public void migrate(@NonNull SupportSQLiteDatabase db) {
             db.execSQL("ALTER TABLE emotionTags ADD COLUMN type INTEGER NOT NULL DEFAULT 1");
+        }
+    };
+
+    //段落表添加段落 ID 的索引
+    public static final Migration MIGRATION_2_3 = new Migration(2,3) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase db) {
+            db.execSQL("CREATE INDEX IF NOT EXISTS `index_paragraphs_paragraphId` ON `paragraphs` (`paragraphId`)");
         }
     };
 }
