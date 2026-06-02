8fea21712baccab0cf19fdce4c6a33a42ee13b6b
E_zhiyu <E82487318@outlook.com>
2026-06-02 11:29:28 +0800

    修改接收器名称

--- app/src/main/java/com/wanderer/journal/automation/broadcast/DiaryAlarmReceiver.java
+++ app/src/main/java/com/wanderer/journal/automation/broadcast/DiaryAlarmReceiver.java
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class DiaryCheckReceiver extends BroadcastReceiver {
+public class DiaryAlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         //TODO:完成日记检查以及自动安排下一个闹钟的逻辑
