f785868e78931d1994c575bbaac0b67d1f97f31c
E_zhiyu <E82487318@outlook.com>
2026-06-26 15:11:16 +0800

    修改角色关系：“好朋友”->“关系紧密”，“恶劣”->“关系恶劣”

--- app/src/main/java/com/wanderer/journal/auxiliary/enums/dropdown/RoleRelationship.java
+++ app/src/main/java/com/wanderer/journal/auxiliary/enums/dropdown/RoleRelationship.java
@@ -1,11 +1,11 @@
 package com.wanderer.journal.auxiliary.enums.dropdown;
 
 public enum RoleRelationship {
-    BAD("恶劣"),
+    POOR("关系恶劣"),
     NOT_GET_ALONG("处不来"),
     NORMAL("普通"),
     NOT_BAD("还不赖"),
-    FRIEND("好朋友");
+    CLOSE("关系紧密");
     private final String title;
 
     RoleRelationship(String title) {
