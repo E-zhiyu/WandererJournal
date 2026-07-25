90e16ba74f1f94d614e00aff95095c14a98ef058
E_zhiyu <E82487318@outlook.com>
2026-07-25 19:45:09 +0800

    删除无用的枚举类

--- app/src/main/java/com/wanderer/journal/auxiliary/enums/intent/PendingRequestCode.java
+++ app/src/main/java/com/wanderer/journal/auxiliary/enums/intent/PendingRequestCode.java
@@ -1,5 +1,6 @@
 package com.wanderer.journal.auxiliary.enums.intent;
 
 public enum PendingRequestCode {
-    TO_WRITE_DIARY  //跳转至写日记界面
+    REQUEST_SET_DIARY_ALARM,    //设置日记提醒闹钟
+    TO_WRITE_DIARY,             //跳转至写日记界面
 }
